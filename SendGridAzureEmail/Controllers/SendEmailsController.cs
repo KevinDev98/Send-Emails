4daff6b2319fdfe064cea565c8fd4e84079eed86
KevinDev98 <kevdan.devti@gmail.com>
2023-02-23 21:09:50 -0600

    first comit

---
+++ SendGridAzureEmail/Controllers/SendEmailsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc.Routing;
+using EllipticCurve.Utils;
+using Microsoft.Ajax.Utilities;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SendGridAzureEmail.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SendGridAzureEmail.Controllers
+{
+    [Route("api/SendGridAzureEmail")]
+    public class SendEmailsController : ApiController
+    {
+        [HttpPost]
+        [Route("SendProcCompletNoti")]
+        public async Task SendProcCompletNoti(EmailModel parametros)
+
+        {
+            string keySG = "DtoTSqG3OJ18gPXAw.aQLsI1bCkitm4pQnob6Gv4f7ehlx2H3lmDR5vrouroc";
+            SendGridClient client = new SendGridClient(keySG);
+
+            //var from2 = new EmailAddress("test1@example.com", "Example User 1");
+            //EmailAddress(«test2@example.com», «Example User 2»)
+            var from = new EmailAddress(parametros.EmailsAddressFROM);
+            List<EmailAddress> Para = parametros.EmailsAddressTO;
+            var subject = parametros.Subject;
+
+            var htmlContent = "< strong > Hello world with HTML content</ strong >";
+            var plaintextContent = "Hello world with TEXT content";
+            bool displayRecipients = false; // set this to true if you want recipients to see each others mail id
+
+            var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, Para, subject, plaintextContent, htmlContent, displayRecipients);
+
+            var response = await client.SendEmailAsync(msg);
+
+        }
+    }
+}
---
+++ SendGridAzureEmail/Models/EmailModel.cs
@@ -0,0 +1,21 @@
+﻿using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendGridAzureEmail.Models
+{
+    public class EmailModel
+    {
+        public List<Dictionary<string, object>> jsonmessage { get; set; }
+        public string Messagge { get; set; }
+        public List<EmailAddress> EmailsAddressTO { get; set; }
+        public string EmailsAddressFROM { get; set; }
+        public string Subject { get; set; }
+        public bool displayRecipients { get; set; }
+        public string HTML { get; set; }
+        public string TEXT { get; set; }
+
+    }
+}
